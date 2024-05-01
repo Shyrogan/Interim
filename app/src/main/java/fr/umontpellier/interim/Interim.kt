ce21660d4a49c036e938500b892a1f9c31bfbc4a
Sébastien VIAL <sebastien@shyrogan.fr>
2024-05-01 19:59:23 +0200

    chore: remove useless code/files

--- app/src/main/java/fr/umontpellier/interim/Interim.kt
+++ app/src/main/java/fr/umontpellier/interim/Interim.kt
@@ -1,11 +1,5 @@
 package fr.umontpellier.interim
 
 import android.app.Application
-import com.google.firebase.Firebase
-import com.google.firebase.auth.auth
 
-class InterimApplication: Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
+class Interim: Application()
