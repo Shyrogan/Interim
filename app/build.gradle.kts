96c2f27c11740cc170162bf006def3fc591c6444
Sébastien VIAL <sebastien@shyrogan.fr>
2024-05-01 19:56:31 +0200

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.kotlinAndroid)
+    alias(libs.plugins.googleServices)
+}
+
+android {
+    namespace = "fr.umontpellier.interim"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "fr.umontpellier.interim"
+        minSdk = 31
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.9.23"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.storage)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+}
---
+++ app/src/main/java/fr/umontpellier/interim/Interim.kt
@@ -0,0 +1,11 @@
+package fr.umontpellier.interim
+
+import android.app.Application
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+
+class InterimApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
