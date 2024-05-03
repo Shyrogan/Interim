07160d3bb1d4e1b037ba068ea4cfa1f0efe2af4a
Selj <samy.el-jaafari@etu.umontpellier.fr>
2024-05-03 16:45:10 +0200

    feat: Bottom Bar de navigation

---
+++ app/src/main/java/fr/umontpellier/interim/component/navigation.kt
@@ -0,0 +1,33 @@
+package fr.umontpellier.interim.component
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import fr.umontpellier.interim.Routes
+
+
+@Composable
+fun BottomNavigationBar(navController: NavHostController) {
+    val items = listOf(Routes.Home, Routes.Search, Routes.Account)
+    BottomNavigation {
+        val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
+        items.forEach { screen ->
+            BottomNavigationItem(
+                icon = { Icon(screen.icon!!, contentDescription = null) },
+                selected = currentRoute == screen.route,
+                onClick = {
+                    if (currentRoute != screen.route) {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id)
+                            launchSingleTop = true
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/fr/umontpellier/interim/layout.kt
+++ app/src/main/java/fr/umontpellier/interim/layout.kt
@@ -1,49 +1,66 @@
 package fr.umontpellier.interim
 
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import fr.umontpellier.interim.component.BottomNavigationBar
 import fr.umontpellier.interim.screen.Home
+import fr.umontpellier.interim.screen.SignIn
 import fr.umontpellier.interim.screen.SignUp
 import fr.umontpellier.interim.screen.signup.SignUpCandidate
 import fr.umontpellier.interim.screen.signup.SignUpChoice
+
 
 val LocalNavHost = compositionLocalOf<NavHostController> {
     error("No NavHostController found")
 }
 
-sealed class Routes(val route: String) {
-    data object Home: Routes("home")
-    data object SignIn: Routes("sign-in")
-    data object SignUp: Routes("sign-up") {
-        data object Choice: Routes("sign-up-choice")
-        data object Candidate: Routes("sign-up-candidate")
-        data object Employer: Routes("sign-up-employer")
-    }
+
+sealed class Routes(val route: String, val icon: ImageVector? = null) {
+    object Home : Routes("home", Icons.Filled.Home)
+    object Search : Routes("search", Icons.Filled.Search)
+    object Account : Routes("account", Icons.Filled.AccountCircle)
+    object SignIn : Routes("sign-in")
+    object SignUp : Routes("sign-up")
+    object SignUpChoice : Routes("sign-up-choice")
+    object SignUpCandidate : Routes("sign-up-candidate")
+    object SignUpEmployer : Routes("sign-up-employer")
 }
+
 
 @Composable
 fun InterimApp() {
     val navController = rememberNavController()
 
     CompositionLocalProvider(LocalNavHost provides navController) {
-        NavHost(navController = navController, Routes.Home.route) {
-            composable(Routes.Home.route) {
-                Home()
-            }
-            composable(Routes.SignIn.route) {
-                Home()
-            }
-            composable(Routes.SignUp.route) {
-                SignUp()
-            }
-            composable(Routes.SignUp.Choice.route) {
-                SignUpChoice()
-            }
-            composable(Routes.SignUp.Candidate.route) {
-                SignUpCandidate()
+        Scaffold(
+            bottomBar = { BottomNavigationBar(navController) }
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = Routes.Home.route,
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                composable(Routes.Home.route) { Home() }
+                composable(Routes.Search.route) { SignUpChoice() }//Provisoire pour tester la bar
+                composable(Routes.Account.route) { SignIn() }//Provisoire pour tester la bar
+
+                composable(Routes.SignIn.route) { SignIn() }
+                composable(Routes.SignUp.route) { SignUp() }
+                composable(Routes.SignUpChoice.route) { SignUpChoice() }
+                composable(Routes.SignUpCandidate.route) { SignUpCandidate() }
             }
         }
     }
